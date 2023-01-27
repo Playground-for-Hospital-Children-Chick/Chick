4682c371c54e48094a8580039bcd784e41a76451
juseong <kjschocolate@gmail.com>
2023-01-27 15:20:22 +0900

    [ADD]: UserRegisterPostReq 추가 및 회원 가입 구현중

--- backend/src/main/java/com/school/chick/controller/UserController.java
+++ backend/src/main/java/com/school/chick/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.school.chick.controller;
 
+import com.school.chick.domain.dto.BaseResponseBody;
 import com.school.chick.domain.entity.User;
 import com.school.chick.service.UserService;
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +23,22 @@ public class UserController {
 
     @Autowired
     private final UserService userService;
-
+//    @PostMapping()
+//    @ApiOperation(value="회원 가입", notes = "아이디와 패스워드를 통해 회원가입 한다")
+//    @ApiResponses({
+//            @ApiResponse(code = 200, message = "성공"),
+//            @ApiResponse(code = 401, message = "가입 실패"),
+//            @ApiResponse(code = 404, message = "반환값 없음"),
+//            @ApiResponse(code = 500, message = "서버 오류"),
+//    })
+//    public ResponseEntity<? extends BaseResponseBody> register(@RequestBody @ApiParam(value="회원가입 정보", required = true)UserRegisterPostReq registerInfo) {
+//        if(userService.createUser(registerInfo)) {
+//            // 가입 성공시 성공 리턴
+//            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+//        }
+//        // 가입 실패시 실패 리턴
+//        return ResponseEntity.status(401).body(BaseResponseBody.of(401, "Failure"));
+//    }
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
