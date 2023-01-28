841d70d0fb798a2138771a81fa5b47da1c4c61f8
엄희원 <eom9300@hanmail.net>
2023-01-28 23:18:57 +0900

    [ADD] component debugging, styled-component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ClickToComponent } from "click-to-react-component";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ClickToComponent />
   </React.StrictMode>,
 )
