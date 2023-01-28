da24c4e319f30b8720d614ca932635d9e8264246
최정온 <chjo0330@gmail.com>
2023-01-29 01:49:48 +0900

    [ADD] atom-CommonBtn

--- frontend/src/components/atoms/CommonBtn/index.jsx
+++ frontend/src/components/atoms/CommonBtn/index.jsx
@@ -1,11 +1,48 @@
-import { StyledCommonBtn } from "./CommonBtn.styled";
-
-const CommonBtn = ({ buttonName }) => {
-  return <StyledCommonBtn>{buttonName}</StyledCommonBtn>;
-};
-
-CommonBtn.defaultProps = {
-  buttonName: "default",
-};
+function CommonBtn() {
+  return (
+    <div className="w-[178px] h-[470px] relative overflow-hidden rounded-[5px] border border-[#9747ff] border-dashed">
+      <div
+        className="w-[138px] h-[70px] absolute left-5 top-5 overflow-hidden rounded-[30px] bg-white"
+        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      >
+        <p className="w-[120px] h-[37px] absolute left-[9px] top-[17px] text-[31px] text-center text-black/[0.66]">
+          Button
+        </p>
+      </div>
+      <div
+        className="w-[138px] h-[70px] absolute left-5 top-[110px] overflow-hidden rounded-[30px] bg-[#57a2e8]"
+        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      >
+        <p className="w-[120px] h-[37px] absolute left-[9px] top-[17px] text-[31px] text-center text-black/[0.66]">
+          Button
+        </p>
+      </div>
+      <div
+        className="w-[138px] h-[70px] absolute left-5 top-[200px] overflow-hidden rounded-[30px] bg-[#9acfc5]"
+        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      >
+        <p className="w-[120px] h-[37px] absolute left-[9px] top-[17px] text-[31px] text-center text-black/[0.66]">
+          Button
+        </p>
+      </div>
+      <div
+        className="w-[138px] h-[70px] absolute left-5 top-[290px] overflow-hidden rounded-[30px] bg-[#ff82b3]"
+        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      >
+        <p className="w-[120px] h-[37px] absolute left-[9px] top-[17px] text-[31px] text-center text-black/[0.66]">
+          Button
+        </p>
+      </div>
+      <div
+        className="w-[138px] h-[70px] absolute left-5 top-[380px] overflow-hidden rounded-[30px] bg-[#fcff83]"
+        style={{ boxShadow: "0px 4px 4px 0 rgba(0,0,0,0.25)" }}
+      >
+        <p className="w-[120px] h-[37px] absolute left-[9px] top-[17px] text-[31px] text-center text-black/[0.66]">
+          Button
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default CommonBtn;
