6dc7e18d7999e79d4309ae8b63ad9628ce38050e
최정온 <chjo0330@gmail.com>
2023-01-29 22:10:36 +0900

    [ADD] SideBar 추가

---
+++ frontend/src/components/atoms/SideBar/index.jsx
@@ -0,0 +1,24 @@
+{
+  /* 
+  최초 작성자: 최정온
+  수정 작성자: 최정온
+  최초 작성일: 23.01.29
+  수정 작성일: 23.01.29
+  
+  Ver 1.0.0
+  
+  - 사용 예시:
+<SideBar />
+  */
+}
+
+function SideBar() {
+  return (
+    <div
+      className="w-[118px] h-[834px] overflow-hidden rounded-[40px] bg-white"
+      style={{ boxShadow: "0px 4px 4px 4px rgba(0,0,0,0.25)" }}
+    />
+  );
+}
+
+export default SideBar;
