3d133c0048fdd02209042824cea661586a1cf938
최정온 <chjo0330@gmail.com>
2023-01-30 00:28:40 +0900

    [UPDATE] SideBar 주석 추가

--- frontend/src/components/molecules/SideBar/index.jsx
+++ frontend/src/components/molecules/SideBar/index.jsx
@@ -1,3 +1,17 @@
+{
+  /* 
+    최초 작성자: 최정온
+    수정 작성자: 최정온
+    최초 작성일: 23.01.29
+    수정 작성일: 23.01.29
+    
+    Ver 1.0.0
+    
+    - 사용 예시:
+      <SideBar />
+    */
+}
+
 import { ReactComponent as LogoText } from "../../../assets/logo/logo-text.svg";
 import SideBarBase from "../../atoms/SideBarBase";
 import SideBarBtn from "./../../atoms/SideBarBtn/index";
