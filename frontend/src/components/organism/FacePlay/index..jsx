eedc19f4b05c97d7dfef1204e7d496ec51772d31
최정온 <chjo0330@gmail.com>
2023-01-30 17:40:52 +0900

    [UPDATE] HomeBox, FacePlay Update

--- frontend/src/components/organism/FacePlay/index..jsx
+++ frontend/src/components/organism/FacePlay/index..jsx
@@ -3,8 +3,8 @@ import CommonBtn from "./../../atoms/CommonBtn/index";
 
 function FacePlay({ children }) {
   return (
-    <div className="flow flow-col w-[1200px] h-[100%]">
-      <div className="flow flow-row">
+    <div className="flow flow-col w-[1076px] h-[100%]">
+      <div className="flex justify-end">
         <CommonBtn text={"회원가입"} />
         <CommonBtn text={"로그인"} color="yellow" />
       </div>
