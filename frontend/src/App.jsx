b874de1154e8f070f3ece5b5bb94ee1455df0d47
엄희원 <eom9300@hanmail.net>
2023-01-30 10:47:41 +0900

    [ADD] axios, openvidu 패키지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 import CommonBtn from "../src/components/atoms/CommonBtn";
 import InputBox from "./components/atoms/Input";
