6b5ef6a9fadf8fdd25889d06b6ba3e860e6c188c
최정온 <chjo0330@gmail.com>
2023-01-30 21:44:03 +0900

    [UPDATE] App Clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-
 import "./App.css";
-import CommonBtn from "../src/components/atoms/CommonBtn";
-import InputBox from "./components/atoms/Input";
-import Video from "./webRTC/Video";
-import WebCamBoardWithCams from "./components/molecules/WebCamBoardWithCams/index";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <CommonBtn buttonName={"처음 만든 버튼"} />
-      <InputBox />
-      <Video />
-      <WebCamBoardWithCams />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
