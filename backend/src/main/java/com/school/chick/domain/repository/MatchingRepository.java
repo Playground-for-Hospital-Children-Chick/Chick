4e8673184617de35629b248e69b288d1116a5e46
juseong <kjschocolate@gmail.com>
2023-01-30 22:35:04 +0900

    [ADD]: 매칭방 관련 entity, dto, service, controller 생성 및 수정

--- backend/src/main/java/com/school/chick/domain/repository/MatchingRepository.java
+++ backend/src/main/java/com/school/chick/domain/repository/MatchingRepository.java
@@ -2,6 +2,8 @@ package com.school.chick.domain.repository;
 
 import com.school.chick.domain.entity.Matching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
 }
