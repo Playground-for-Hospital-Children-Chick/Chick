b164085129b44326b5eb64a1eeda9a1b264b36b4
gofiction95 <gofiction95@gmail.com>
2023-01-30 13:17:38 +0900

    [UPDATE] atom-Input, atom-CheckBox

---
+++ frontend/src/components/atoms/CheckBox/index.jsx
@@ -0,0 +1,10 @@
+function CheckBox() {
+  return (
+    <>
+      <div>
+        <input type="radio" id="unchecked" name="unchecked" value="label" />
+        <label for="unchecked">label</label>
+      </div>
+    </>
+  );
+}
