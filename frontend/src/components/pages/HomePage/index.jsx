1a5204e4339eb58fe818b41a1d90d1087579465e
최정온 <chjo0330@gmail.com>
2023-01-30 21:39:54 +0900

    [UPDATE] HomePage Update

--- frontend/src/components/pages/HomePage/index.jsx
+++ frontend/src/components/pages/HomePage/index.jsx
@@ -1,11 +1,15 @@
 import SideBar from "../../molecules/SideBar";
 import FacePlay from "../../organism/FacePlay/index.";
+import React from "react";
+import RiceEat from "./../../organism/RiceEat/index";
 
 function HomePage({ children }) {
+  let [index, setIndex] = React.useState(0);
+
   return (
     <div className="flex flex-row">
-      <SideBar />
-      <FacePlay />
+      <SideBar index={index} setIndex={setIndex} />
+      {index == 0 ? <FacePlay /> : <RiceEat />}
     </div>
   );
 }
