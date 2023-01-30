8b1ea8e14ea359bda00c5c7e5a911ff2c0789d42
최정온 <chjo0330@gmail.com>
2023-01-30 16:12:12 +0900

    [UPDATE] HomeBox, FacePlayHomeBox

--- frontend/src/components/molecules/FacePlayHomeBox/index.jsx
+++ frontend/src/components/molecules/FacePlayHomeBox/index.jsx
@@ -13,23 +13,27 @@
 }
 
 import HomeBox from "../../atoms/HomeBox";
-import { ReactComponent as ChildOne } from "../../../assets/images/faceplay/child_one.svg";
-import { ReactComponent as ChildTwo } from "../../../assets/images/faceplay/child_two.svg";
-import { ReactComponent as ChildThree } from "../../../assets/images/faceplay/child_three.svg";
-import { ReactComponent as ChildFour } from "../../../assets/images/faceplay/child_four.svg";
+import ChildOne from "../../../assets/images/faceplay/child_one.svg";
+import ChildTwo from "../../../assets/images/faceplay/child_two.svg";
+import ChildThree from "../../../assets/images/faceplay/child_three.svg";
+import ChildFour from "../../../assets/images/faceplay/child_four.svg";
 import GamePlayBtn from "./../../atoms/GamePlayBtn/index";
 
 function FacePlayHomeBox() {
   return (
     <HomeBox>
-      <p className="font-chick text-4xl text-center text-black/[0.66]">
+      <div className="font-chick text-4xl text-center text-black/[0.66]">
         친구들과 밥 먹기!
-      </p>
-      <ChildOne />
-      <ChildTwo />
-      <ChildThree />
-      <ChildFour />
-      <GamePlayBtn text={"친구들 만나러 가기"} color="emerald" />
+      </div>
+      <div className="inline-flex justify-center w-[100%]">
+        <img src={ChildOne} style={{ height: 87, width: 154 }} />
+        <img src={ChildTwo} style={{ height: 87, width: 154 }} />
+        <img src={ChildThree} style={{ height: 87, width: 154 }} />
+        <img src={ChildFour} style={{ height: 87, width: 154 }} />
+      </div>
+      <div>
+        <GamePlayBtn text={"친구들 만나러 가기"} color="emerald" />
+      </div>
     </HomeBox>
   );
 }
