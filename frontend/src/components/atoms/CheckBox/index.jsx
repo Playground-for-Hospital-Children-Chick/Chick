77d4900628aa5a9c78f7f5077a158dc398407444
gofiction95 <gofiction95@gmail.com>
2023-01-30 16:50:59 +0900

    [UPDATE] atom-CheckBox 글씨체변경

--- frontend/src/components/atoms/CheckBox/index.jsx
+++ frontend/src/components/atoms/CheckBox/index.jsx
@@ -3,7 +3,9 @@ function CheckBox() {
     <>
       <div>
         <input type="radio" id="unchecked" name="unchecked" value="label" />
-        <label htmlFor="unchecked">label</label>
+        <label className="font-chick" htmlFor="unchecked">
+          label
+        </label>
       </div>
     </>
   );
