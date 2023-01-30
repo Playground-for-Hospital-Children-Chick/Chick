cb678c2c60b88aae46ec83283cbb53e7d461d14b
엄희원 <eom9300@hanmail.net>
2023-01-30 13:22:53 +0900

    [ADD] webRTC test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
+
 import "./App.css";
 import CommonBtn from "../src/components/atoms/CommonBtn";
 import InputBox from "./components/atoms/Input";
+import Video from "./webRTC/Video";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +13,7 @@ function App() {
     <div className="App">
       <CommonBtn buttonName={"처음 만든 버튼"} />
       <InputBox />
+      <Video/>
     </div>
   );
 }
