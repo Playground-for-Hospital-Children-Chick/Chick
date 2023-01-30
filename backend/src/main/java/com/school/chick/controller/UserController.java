b3cd9855b62f7bdf021cb20e45fad23c2801b62b
juseong <kjschocolate@gmail.com>
2023-01-30 12:03:04 +0900

    [UPDATE]: 회운 가입 구현중

--- backend/src/main/java/com/school/chick/controller/UserController.java
+++ backend/src/main/java/com/school/chick/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.school.chick.controller;
 
 import com.school.chick.domain.dto.BaseResponseBody;
+import com.school.chick.domain.dto.UserRegisterPostReq;
 import com.school.chick.domain.entity.User;
 import com.school.chick.service.UserService;
 import io.swagger.annotations.*;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,32 +15,33 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(value = "유저 API", tags = {"User"})
 @RestController
 @RequestMapping("/users")
-@Api("회원 관리 퀀트롤러 API V1")
 public class UserController {
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private static final String SUCCESS = "success";
-    private static final String FAIL = "fail";
 
     @Autowired
     private final UserService userService;
-//    @PostMapping()
-//    @ApiOperation(value="회원 가입", notes = "아이디와 패스워드를 통해 회원가입 한다")
-//    @ApiResponses({
-//            @ApiResponse(code = 200, message = "성공"),
-//            @ApiResponse(code = 401, message = "가입 실패"),
-//            @ApiResponse(code = 404, message = "반환값 없음"),
-//            @ApiResponse(code = 500, message = "서버 오류"),
-//    })
-//    public ResponseEntity<? extends BaseResponseBody> register(@RequestBody @ApiParam(value="회원가입 정보", required = true)UserRegisterPostReq registerInfo) {
-//        if(userService.createUser(registerInfo)) {
-//            // 가입 성공시 성공 리턴
-//            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
-//        }
-//        // 가입 실패시 실패 리턴
-//        return ResponseEntity.status(401).body(BaseResponseBody.of(401, "Failure"));
-//    }
+    @PostMapping("/register")
+    @ApiOperation(value="회원 가입", notes = "아이디와 패스워드를 통해 회원가입 한다")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "가입 실패"),
+            @ApiResponse(code = 404, message = "반환값 없음"),
+            @ApiResponse(code = 500, message = "서버 오류"),
+    })
+    public ResponseEntity<? extends BaseResponseBody> register(@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
+        System.out.println("회원가입 입니다");
+        System.out.println("회원가입 입니다");
+        System.out.println("회원가입 입니다");
+        System.out.println(registerInfo.toString());
+        if(userService.createUser(registerInfo)) {
+            // 가입 성공시 성공 리턴
+            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+        }
+        // 가입 실패시 실패 리턴
+        return ResponseEntity.status(401).body(BaseResponseBody.of(401, "Failure"));
+    }
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
--- backend/src/main/java/com/school/chick/service/Impl/UserServiceImpl.java
+++ backend/src/main/java/com/school/chick/service/Impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.school.chick.service.Impl;
 
 import com.school.chick.domain.dto.UserRegisterPostReq;
+import com.school.chick.domain.entity.Profile;
 import com.school.chick.domain.entity.User;
+import com.school.chick.domain.repository.ProfileRepository;
 import com.school.chick.domain.repository.UserRepository;
 import com.school.chick.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+//    private final ProfileRepository profileRepository;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
@@ -24,23 +27,28 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
-//    public boolean createUser(UserRegisterPostReq userRegisterInfo) {
-////        if (userRepository.findByPhone(userRegisterInfo.getPhone()) != null) {
-////            return false;
-////        }
-//
-//        User user = new User();
-//        user.setPhone(userRegisterInfo.getPhone());
-//        user.setPassword(passwordEncoder.encode(userRegisterInfo.getPassword()));
-//        user.setName(userRegisterInfo.getName());
-//        user.setAddress(userRegisterInfo.getAddress());
-//        user.setBank(userRegisterInfo.getBank());
-//        user.setAccount(userRegisterInfo.getAccount());
-//        user.setZipCode(userRegisterInfo.getZipCode());
-//        user.setDetailAddress(userRegisterInfo.getDetailAddress());
-//        user.setPicture(imageRepository.findById(userRegisterInfo.getPicture()).get());
-//        user.setData_create(LocalDateTime.now());
-//        userRepository.save(user);
-//        return true;
-//    }
+    public boolean createUser(UserRegisterPostReq userRegisterInfo) {
+//        if (userRepository.findByPhone(userRegisterInfo.getPhone()) != null) {
+//            return false;
+//        }
+
+        User user = new User();
+        user.setProfNo(new Profile(1L, "chick", "/click.jpg", "ssafy", LocalDateTime.now(), "ssafy", LocalDateTime.now()));
+        user.setUserPwd(userRegisterInfo.getUser_password());
+        user.setUserChName(userRegisterInfo.getUser_child_name());
+        user.setUserParentName(userRegisterInfo.getUser_parent_name());
+        user.setUserSex(userRegisterInfo.getUser_sex());
+        user.setUserBirth(userRegisterInfo.getUser_birth());
+        user.setUserState(userRegisterInfo.getUser_state());
+        user.setUserNumberOfReports(userRegisterInfo.getUser_reported());
+        user.setUserServiceTerm(userRegisterInfo.getUser_service_term());
+        user.setUserPrivacyTerm(userRegisterInfo.getUser_privacy_term());
+        user.setUserRole(userRegisterInfo.getUser_role());
+        user.setUserCreateBy(userRegisterInfo.getUser_create_by());
+        user.setUserCreateDate(LocalDateTime.now());
+        user.setUserUpdateBy(userRegisterInfo.getUser_upadate_by());
+        user.setUserUpdateDate(LocalDateTime.now());
+        userRepository.save(user);
+        return true;
+    }
 }
