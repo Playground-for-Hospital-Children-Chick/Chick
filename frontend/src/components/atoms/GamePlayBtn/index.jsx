16d36d72a73c94e03e8555bca75e3d7ea2fb4495
최정온 <chjo0330@gmail.com>
2023-01-30 10:28:29 +0900

    [UPDATE] GamePlayBtn 버튼 수정

--- frontend/src/components/atoms/GamePlayBtn/index.jsx
+++ frontend/src/components/atoms/GamePlayBtn/index.jsx
@@ -3,7 +3,7 @@
         최초 작성자: 최정온
         수정 작성자: 최정온
         최초 작성일: 23.01.29
-        수정 작성일: 23.01.29
+        수정 작성일: 23.01.30
         
         Ver 1.0.0
         
@@ -21,50 +21,79 @@ default="blue"
 }
 
 function GamePlayBtn({ text, color, onClick }) {
-  return (
-    <div className="w-[479px] h-[495px] relative overflow-hidden rounded-[5px] border border-[#9747ff] border-dashed">
-      <div
-        className="flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-white"
+  if (color === "white") {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-white"
         style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
       >
         <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
-          GamePlayBtn
+          {text}
         </p>
-      </div>
-      <div
-        className="flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-[115px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#6bb8ff]"
+      </button>
+    );
+  } else if (color === "pink") {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#ffcadf]"
         style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
       >
         <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
-          GamePlayBtn
+          {text}
         </p>
-      </div>
-      <div
-        className="flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-[210px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#b2e9de]"
+      </button>
+    );
+  } else if (color === "yellow") {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#fdffb6]"
         style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
       >
         <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
-          GamePlayBtn
+          {text}
         </p>
-      </div>
-      <div
-        className="flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-[305px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#ffcadf]"
+      </button>
+    );
+  } else if (color === "emerald") {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#b2e9de]"
         style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
       >
         <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
-          GamePlayBtn
+          {text}
         </p>
-      </div>
-      <div
-        className="flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-[400px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#fdffb6]"
+      </button>
+    );
+  } else if (color === "blue") {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#6bb8ff]"
         style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
       >
         <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
-          GamePlayBtn
+          {text}
         </p>
-      </div>
-    </div>
-  );
+      </button>
+    );
+  } else {
+    return (
+      <button
+        className="font-chick flex justify-center items-center w-[439px] h-[75px] absolute left-5 top-5 overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#6bb8ff]"
+        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+        onClick={onClick}
+      >
+        <p className="flex-grow-0 flex-shrink-0 w-[209.87px] text-[28px] text-center text-black/[0.66]">
+          {text}
+        </p>
+      </button>
+    );
+  }
 }
 
 export default GamePlayBtn;
