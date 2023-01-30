d885e2d0e980141db52d6d6fb66a25890668c247
최정온 <chjo0330@gmail.com>
2023-01-30 21:38:06 +0900

    [UPDATE] SideBar 업데이트

--- frontend/src/components/molecules/SideBar/index.jsx
+++ frontend/src/components/molecules/SideBar/index.jsx
@@ -3,12 +3,15 @@
     최초 작성자: 최정온
     수정 작성자: 최정온
     최초 작성일: 23.01.29
-    수정 작성일: 23.01.29
+    수정 작성일: 23.01.30
     
     Ver 1.0.0
+    최초 버전
+    Ver 1.1.0
+    index, setIndex Props 추가
     
     - 사용 예시:
-      <SideBar />
+    <SideBar index={index} setIndex={setIndex} />
     */
 }
 
@@ -16,20 +19,32 @@ import LogoText from "../../../assets/logo/logo-text.svg";
 import SideBarBase from "../../atoms/SideBarBase";
 import SideBarBtn from "./../../atoms/SideBarBtn/index";
 
-function SideBar() {
+function SideBar({ index, setIndex }) {
   return (
     <SideBarBase>
       <img src={LogoText} />
-      <SideBarBtn
-        type={"tiger"}
-        selected={true}
-        onClick={() => console.log("Clicked")}
-      />
-      <SideBarBtn
-        type={"rice"}
-        selected={false}
-        onClick={() => console.log("Clicked")}
-      />
+      {index == 0 ? (
+        <SideBarBtn
+          type={"tiger"}
+          selected={true}
+          onClick={() => setIndex(0)}
+        />
+      ) : (
+        <SideBarBtn
+          type={"tiger"}
+          selected={false}
+          onClick={() => setIndex(0)}
+        />
+      )}
+      {index == 1 ? (
+        <SideBarBtn type={"rice"} selected={true} onClick={() => setIndex(1)} />
+      ) : (
+        <SideBarBtn
+          type={"rice"}
+          selected={false}
+          onClick={() => setIndex(1)}
+        />
+      )}
     </SideBarBase>
   );
 }
