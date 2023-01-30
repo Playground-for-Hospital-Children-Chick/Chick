ab3e256bae64d27c1a70f0d48ee540e8345af760
엄희원 <eom9300@hanmail.net>
2023-01-30 22:22:35 +0900

    [UPDATE] atoms-MicBtn 주석

--- frontend/src/components/atoms/MicBtn/index.jsx
+++ frontend/src/components/atoms/MicBtn/index.jsx
@@ -1,3 +1,19 @@
+{
+  /* 
+최초 작성자: 엄희원
+수정 작성자: 엄희원
+최초 작성일: 23.01.30
+수정 작성일: 23.01.30
+
+Ver 1.0.0
+
+- 사용 예시: 마이크 on/off
+  
+- 색깔
+default="emerald"
+*/
+}
+
 import MicOn from "./../../../assets/images/faceplay/micOn.svg";
 import MicOff from "./../../../assets/images/faceplay/micOff.svg";
 import React, { useState } from "react";
