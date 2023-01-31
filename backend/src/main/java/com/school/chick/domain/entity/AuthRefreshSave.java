77f0a2325c240a9657a85312b106122f79c9dbd1
juseong <kjschocolate@gmail.com>
2023-01-31 13:20:30 +0900

    [UPDATE]: 리프레쉬토큰 엔티티 수정

--- backend/src/main/java/com/school/chick/domain/entity/AuthRefreshSave.java
+++ backend/src/main/java/com/school/chick/domain/entity/AuthRefreshSave.java
@@ -3,8 +3,14 @@ package com.school.chick.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -12,9 +18,25 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class AuthRefreshSave {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
+    @Column(name ="AUTH_ID", columnDefinition = "INT UNSIGNED")
+    private int id;
 
-    @Column
-    private String refreshToken;
+    private String authEmail;
+    private String token;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String authCreateBy;
+    @CreatedDate
+    @Column(updatable = false)
+    @Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+    private LocalDateTime authCreateDate;
+    @LastModifiedBy
+    private String authUpdateBy;
+    @LastModifiedDate
+    @Column(nullable = false)
+    @Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+    private LocalDateTime authUpdateDate;
 }
