123ab98df057444198b8fa3855cede67d66e2d39
juseong <kjschocolate@gmail.com>
2023-02-01 14:59:11 +0900

    [ADD] Room Repository 추가

--- backend/src/main/java/com/school/chick/domain/repository/MatchingRepository.java
+++ backend/src/main/java/com/school/chick/domain/repository/MatchingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface MatchingRepository extends JpaRepository<Matching, Long> {
+public interface MatchingRepository extends JpaRepository<Matching, Integer> {
 }
