47051f3a4d1b70210adbd7acea4be4e75cf3e646
juseong <kjschocolate@gmail.com>
2023-02-01 15:59:33 +0900

    [ADD] 방테이블에서 게임종류로 선택 후 방 인워을 기존으로 오롬차순으로 정렬하여 리턴하는 JPA 추가

--- backend/src/main/java/com/school/chick/domain/repository/RoomRepository.java
+++ backend/src/main/java/com/school/chick/domain/repository/RoomRepository.java
@@ -3,5 +3,8 @@ package com.school.chick.domain.repository;
 import com.school.chick.domain.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
+
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    ArrayList<Room> findByRoomTypeOrderByRoomCntDesc(String roomType);
 }
