d695abe4fc41082153b3be4819725fed0717e6d4
juseong <kjschocolate@gmail.com>
2023-02-01 16:40:23 +0900

    [ADD] 참여할 수 있는 게임방이 있으면 해당방의 세션을 사용하고 참여할 수 있는 방이 없으면 새로운 새션을 생성

--- backend/src/main/java/com/school/chick/domain/repository/RoomRepository.java
+++ backend/src/main/java/com/school/chick/domain/repository/RoomRepository.java
@@ -2,9 +2,11 @@ package com.school.chick.domain.repository;
 
 import com.school.chick.domain.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
+@Component
 public interface RoomRepository extends JpaRepository<Room, Integer> {
     ArrayList<Room> findByRoomTypeOrderByRoomCntDesc(String roomType);
 }
