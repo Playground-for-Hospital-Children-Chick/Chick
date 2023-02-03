cde074ea818144d7cd1a718a81895af30910dd34
최정온 <chjo0330@gmail.com>
2023-02-03 10:12:27 +0900

    [UPDATE] 여러 Componet 업데이트

--- frontend/src/components/organism/FacePlay/index..jsx
+++ frontend/src/components/organism/FacePlay/index..jsx
@@ -1,12 +1,26 @@
-import FacePlayHomeBox from "../../molecules/FacePlayHomeBox";
-import CommonBtn from "./../../atoms/CommonBtn/index";
+{
+  /* 
+      최초 작성자: 최정온
+      수정 작성자: 최정온
+      최초 작성일: 23.01.30
+      수정 작성일: 23.02.02
+      
+      Ver 1.0.0
+      
+      - 사용 예시:
+        <FacePlay />
+      */
+}
+
+import FacePlayHomeBox from '../../molecules/FacePlayHomeBox';
+import CommonBtn from './../../atoms/CommonBtn/index';
 
 function FacePlay({ children }) {
   return (
-    <div className="flow flow-col w-[1076px] h-[100%]">
+    <div className="absolute left-48 w-[1076px] h-[100%]">
       <div className="flex justify-end">
-        <CommonBtn text={"회원가입"} />
-        <CommonBtn text={"로그인"} color="yellow" />
+        <CommonBtn text={'회원가입'} />
+        <CommonBtn text={'로그인'} color="yellow" />
       </div>
       <FacePlayHomeBox />
     </div>
