623ac7e4df64d93badc5b1e565d95898970e67ba
juseong <kjschocolate@gmail.com>
2023-02-05 20:26:04 +0900

    [ADD] 게임방 생성

--- backend/src/main/java/com/ssafy/api/domain/entity/Room.java
+++ backend/src/main/java/com/ssafy/api/domain/entity/Room.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @ToString
 @Builder
 @AllArgsConstructor
--- backend/src/main/java/com/ssafy/api/service/RoomService.java
+++ backend/src/main/java/com/ssafy/api/service/RoomService.java
@@ -4,6 +4,6 @@ package com.ssafy.api.service;
 import java.util.Map;
 
 public interface RoomService {
-    public String getRoomSession(String gameType);
+    public String getRoomSession(String email, String gameType);
     public void createMachingInfo(Map<String, Object> params, String userSession);
 }
