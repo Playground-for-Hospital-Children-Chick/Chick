b84e4663de12e568670b83aa8f5856c18c6baf96
엄희원 <eom9300@hanmail.net>
2023-02-05 20:19:38 +0900

    [UPDATE] apply ar on webRTC

--- frontend/src/webRTC/Video.jsx
+++ frontend/src/webRTC/Video.jsx
@@ -2,10 +2,15 @@ import { OpenVidu } from "openvidu-browser";
 import axios from "axios";
 import React, { Component } from "react";
 import UserVideoComponent from "./UserVideoComponent";
+import ArBottomBarBase from "../components/atoms/ArBottomBarBase";
+import WebCamBoard from "../components/atoms/WebCamBoard";
+import FriendIsComing from "../components/atoms/FriendIsComing";
+import MicBtn from "../components/atoms/MicBtn";
+import VideoBtn from "../components/atoms/VideoBtn";
 
-// const APPLICATION_SERVER_URL = "http://localhost:5000/";
+const APPLICATION_SERVER_URL = "http://localhost:5000/";
 // "http://localhost:5000/";
-const APPLICATION_SERVER_URL = "https://i8b207.p.ssafy.io:9000/";
+// const APPLICATION_SERVER_URL = "http://3.35.166.44:9000/";
 
 class Video extends Component {
   constructor(props) {
@@ -19,6 +24,7 @@ class Video extends Component {
       publisher: undefined,
       subscribers: [],
       mediaStream: undefined,
+      stream: undefined,
     };
 
     this.joinSession = this.joinSession.bind(this);
@@ -28,16 +34,37 @@ class Video extends Component {
     this.onbeforeunload = this.onbeforeunload.bind(this);
     this.micStatusChanged = this.micStatusChanged.bind(this);
     this.camStatusChanged = this.camStatusChanged.bind(this);
+    this.applyDeepAR = this.applyDeepAR.bind(this);
     this.canvasRef = document.createElement("canvas");
+  }
+
+  async applyDeepAR() {
+    this.startDeepAR(this.canvasRef);
+    await this.setState({
+      mediaStream: this.canvasRef.captureStream(),
+    });
+    console.log("mediaStream", this.state.mediaStream);
+    var videoTracks = this.state.mediaStream.getVideoTracks();
+    console.log("videoTracks[0]", videoTracks[0]);
+    console.log(this.state.publisher.properties.videoSource);
+
+    if (this.state.publisher.properties.videoSource == undefined) {
+      this.state.publisher.properties.videoSource = videoTracks[0];
+    } else {
+      this.state.publisher.properties.videoSource = undefined;
+    }
+    console.log(this.state.publisher.properties.videoSource);
+    this.state.session.publish(this.state.publisher);
   }
 
   startDeepAR(canvas) {
     var { DeepAR } = window;
 
     var deepAR = DeepAR({
-      canvasWidth: 500,
-      canvasHeight: 500,
-      licenseKey: "17b3582869e511e992581d53ee247344cfe4ea5b2787852672d14e03a419c3a887dafb093b8aa3ea",
+      canvasWidth: 550,
+      canvasHeight: 307,
+      licenseKey:
+        "17b3582869e511e992581d53ee247344cfe4ea5b2787852672d14e03a419c3a887dafb093b8aa3ea",
       canvas: canvas,
       numberOfFaces: 1,
       libPath: "/lib",
@@ -57,24 +84,27 @@ class Video extends Component {
   }
 
   camStatusChanged() {
-    this.state.publisher.stream.videoActive = !this.state.publisher.stream.videoActive;
+    this.state.publisher.stream.videoActive =
+      !this.state.publisher.stream.videoActive;
     this.state.session.publish(this.state.publisher);
+    // this.state.session.publish();
+    console.log(this.state);
   }
 
   micStatusChanged() {
-    this.state.publisher.stream.audioActive = !this.state.publisher.stream.audioActive;
+    this.state.publisher.stream.audioActive =
+      !this.state.publisher.stream.audioActive;
     this.state.session.publish(this.state.publisher);
   }
 
   componentDidMount() {
     window.addEventListener("beforeunload", this.onbeforeunload);
-    console.log("canvasRef", this.canvasRef);
-    // console.log("canvasRef.current", this.canvasRef.current);
-    var canvasContext = this.canvasRef.getContext("webgl");
-    console.log("canvasContext", canvasContext);
 
-    this.startDeepAR(this.canvasRef);
-    this.joinSession();
+    // console.log("canvasRef.current", this.canvasRef.current);
+    // var canvasContext = this.canvasRef.getContext("webgl");
+
+    // this.startDeepAR(this.canvasRef);
+    // this.joinSession();
   }
 
   componentWillUnmount() {
@@ -114,6 +144,8 @@ class Video extends Component {
     this.OV = new OpenVidu();
 
     // --- 2) Init a session ---
+
+    // this.startDeepAR(this.canvasRef);
 
     this.setState(
       {
@@ -162,19 +194,20 @@ class Video extends Component {
               // --- 5) Get your own camera stream ---
               console.log("connect Session");
 
-              // if (canvasRef) {
-              this.setState({
-                mediaStream: this.canvasRef.captureStream(),
-              });
-              console.log("mediaStream", this.state.mediaStream);
-              var videoTracks = this.state.mediaStream.getVideoTracks();
-              console.log("videoTracks[0]", videoTracks[0]);
+              // ararararar
+              // this.setState({
+              //   mediaStream: this.canvasRef.captureStream(),
+              // });
+              // console.log("mediaStream", this.state.mediaStream);
+              // var videoTracks = this.state.mediaStream.getVideoTracks();
+              // console.log("videoTracks[0]", videoTracks[0]);
 
               // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
               // element: we will manage it on our own) and with the desired properties
               let publisher = await this.OV.initPublisherAsync(undefined, {
                 audioSource: undefined, // The source of audio. If undefined default microphone
-                videoSource: videoTracks[0], // The source of video. If undefined default webcam
+                videoSource: undefined, // The source of video. If undefined default webcam
+                // videoSource: videoTracks[0], // The source of video. If undefined default webcam
                 publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
                 publishVideo: true, // Whether you want to start publishing with your video enabled or not
                 resolution: "555x307", // The resolution of your video
@@ -189,9 +222,16 @@ class Video extends Component {
 
               // Obtain the current video device in use
               var devices = await this.OV.getDevices();
-              var videoDevices = devices.filter((device) => device.kind === "videoinput");
-              var currentVideoDeviceId = publisher.stream.getMediaStream().getVideoTracks()[0].getSettings().deviceId;
-              var currentVideoDevice = videoDevices.find((device) => device.deviceId === currentVideoDeviceId);
+              var videoDevices = devices.filter(
+                (device) => device.kind === "videoinput"
+              );
+              var currentVideoDeviceId = publisher.stream
+                .getMediaStream()
+                .getVideoTracks()[0]
+                .getSettings().deviceId;
+              var currentVideoDevice = videoDevices.find(
+                (device) => device.deviceId === currentVideoDeviceId
+              );
 
               // Set the main video in the page to display our webcam and store our Publisher
               this.setState({
@@ -201,7 +241,11 @@ class Video extends Component {
               // }
             })
             .catch((error) => {
-              console.log("There was an error connecting to the session:", error.code, error.message);
+              console.log(
+                "There was an error connecting to the session:",
+                error.code,
+                error.message
+              );
             });
         });
       }
@@ -230,42 +274,78 @@ class Video extends Component {
   }
 
   render() {
-    const mySessionId = "SessionID";
+    const mySessionId = this.state.mySessionId;
+    const myUserName = this.state.myUserName;
 
     return (
-      <div className="container">
+      <div className="flex justify-center">
         {this.state.session === undefined ? (
-          <div id="join">
-            <div id="img-div">
-              <img src="resources/images/openvidu_grey_bg_transp_cropped.png" alt="OpenVidu logo" />
+          <div>
+            <div>
+              <h1> Join a video session </h1>
+              <form className="form-group" onSubmit={this.joinSession}>
+                <p className="text-center">
+                  <input
+                    className="bg-pink-300 text-3xl"
+                    name="commit"
+                    type="submit"
+                    value="JOIN"
+                  />
+                </p>
+              </form>
             </div>
           </div>
         ) : null}
 
         {this.state.session !== undefined ? (
-          <div id="session">
-            <div id="session-header">
-              <h1 id="session-title">{mySessionId}</h1>
-              <input
-                className="btn btn-large btn-danger"
-                type="button"
-                id="buttonLeaveSession"
-                onClick={this.leaveSession}
-                value="Leave session"
-              />
-            </div>
-
-            <div id="video-container" className="col-md-6">
+          <div>
+            <WebCamBoard>
               {this.state.publisher !== undefined ? (
-                <div className="stream-container col-md-6 col-xs-6">
-                  <UserVideoComponent streamManager={this.state.publisher} />
+                <div className="m-3 rounded-[30px] w-[555px] h-[307px] flex items-center justify-center">
+                  <div class="relative">
+                    <UserVideoComponent streamManager={this.state.publisher} />
+                    <div
+                      class="absolute bottom-0 right-0"
+                      onClick={this.micStatusChanged}
+                    >
+                      <MicBtn />
+                    </div>
+                    <div
+                      class="absolute bottom-0 left-0"
+                      onClick={this.camStatusChanged}
+                    >
+                      <VideoBtn />
+                    </div>
+                  </div>
                 </div>
               ) : null}
-              {this.state.subscribers.map((sub, i) => (
-                <div key={i} className="stream-container col-md-6 col-xs-6">
-                  <UserVideoComponent streamManager={sub} />
+
+              {this.state.subscribers.map((sub, i) =>
+                i < 3 ? <UserVideoComponent streamManager={sub} /> : null
+              )}
+
+              {this.state.subscribers.length === 0 ? <FriendIsComing /> : null}
+              {this.state.subscribers.length === 0 ? <FriendIsComing /> : null}
+              {this.state.subscribers.length === 0 ? <FriendIsComing /> : null}
+
+              {this.state.subscribers.length === 1 ? <FriendIsComing /> : null}
+              {this.state.subscribers.length === 1 ? <FriendIsComing /> : null}
+
+              {this.state.subscribers.length === 2 ? <FriendIsComing /> : null}
+            </WebCamBoard>
+
+            <div className="flex justify-center">
+              <ArBottomBarBase>
+                <div className="flex">
+                  <input
+                    className="bg-pink-400 text-3xl rounded-[30px]"
+                    type="button"
+                    onClick={this.leaveSession}
+                    value="나가기"
+                  />
                 </div>
-              ))}
+                <button onClick={this.applyDeepAR}>ar버튼입니다</button>
+              </ArBottomBarBase>
             </div>
           </div>
         ) : null}
@@ -281,11 +361,7 @@ class Video extends Component {
   async createSession(sessionId) {
     const response = await axios.post(
       APPLICATION_SERVER_URL + "api/sessions",
-      {
-        //customSessionId: sessionId
-        email: "ssafy@ssafy.com",
-        gameType: "face",
-      },
+      { customSessionId: sessionId },
       {
         headers: { "Content-Type": "application/json" },
       }
@@ -299,48 +375,6 @@ class Video extends Component {
     const response = await axios.post(
       APPLICATION_SERVER_URL + "api/sessions/" + sessionId + "/connections",
       {},
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    return response.data; // The token
-  }
-
-  async getToken() {
-    const sessionId = await this.createSession(this.state.mySessionId);
-    return await this.createToken(sessionId);
-  }
-
-  async createSession(sessionId) {
-    const response = await axios.post(
-      APPLICATION_SERVER_URL + "api/sessions",
-      {
-        customSessionId: sessionId,
-        // email: "ssafy@ssafy.com",
-        // gameType: "face",
-      },
-
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    return response.data; // The sessionId
-  }
-
-  async createToken(sessionId) {
-    const response = await axios.post(
-      APPLICATION_SERVER_URL + "api/sessions/" + sessionId + "/connections",
-      {
-        // customIceServers: [
-        //   {
-        //     url: "https://3.35.166.44:4443/",
-        //     username: "OPENVIDUAPP",
-        //     credential: "MY_SECRET",
-        //   },
-        // ],
-      },
       {
         headers: { "Content-Type": "application/json" },
       }
