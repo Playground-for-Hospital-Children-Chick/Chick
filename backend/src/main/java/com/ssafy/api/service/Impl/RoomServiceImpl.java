7da7887fccfb1b4fce066a8217642d6d8260ac57
juseong <kjschocolate@gmail.com>
2023-02-05 20:55:38 +0900

    [UPDATE] 게임방 생성 수정

--- backend/src/main/java/com/ssafy/api/service/Impl/RoomServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/Impl/RoomServiceImpl.java
@@ -31,7 +31,7 @@ public class RoomServiceImpl implements RoomService {
         방의 종류로 방을 선택 후 인원 기준으로 Order by 해서 가장 인원수가 적은 방을 확안한다.
         해당 방이 없거나 인원수가 4 이상이면 새로운 세션을 만들고 그렇지 않으면 해당 방의 세션을 리턴한다
          */
-        ArrayList<Room> roomArrayList = roomRepository.findByRoomTypeOrderByRoomCntDesc(gameType);
+        ArrayList<Room> roomArrayList = roomRepository.findByRoomTypeOrderByRoomCntAsc(gameType);
         if(!roomArrayList.isEmpty() && roomArrayList.get(0).getRoomCnt() < 4) { // 참가할 수 있는 게임방이 있으면 기존 방에 참가
             Room room = roomArrayList.get(0);
             room.setRoomCnt(room.getRoomCnt() + 1); // 방의 인원수 + 1
