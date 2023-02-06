17e08befc14f196c62f2cfb829405c8aea074a7a
최정온 <chjo0330@gmail.com>
2023-02-06 23:33:59 +0900

    [UPDATE] GamePlayBtn 버튼 수정

--- frontend/src/components/atoms/GamePlayBtn/index.jsx
+++ frontend/src/components/atoms/GamePlayBtn/index.jsx
@@ -20,80 +20,25 @@ default="blue"
 */
 }
 
-function GamePlayBtn({ text = "GamePlay", color, setGameStart }) {
-  if (color === "white") {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-white"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  } else if (color === "pink") {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#ffcadf]"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  } else if (color === "yellow") {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#fdffb6]"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  } else if (color === "emerald") {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 rounded-[30px] bg-[#b2e9de]"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  } else if (color === "blue") {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#6bb8ff]"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  } else {
-    return (
-      <button
-        className="font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px] bg-[#6bb8ff]"
-        style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
-        onClick={() => setGameStart(true)}
-      >
-        <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
-          {text}
-        </p>
-      </button>
-    );
-  }
+import classnames from "classnames";
+
+function GamePlayBtn({ text = "GamePlay", color = "bg-blue-300", onClick }) {
+  const classStr = classnames(
+    `font-chick justify-center items-center w-[439px] h-[75px] overflow-hidden gap-1 px-16 py-5 rounded-[30px]`,
+    color
+  );
+
+  return (
+    <button
+      className={classStr}
+      style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+      onClick={onClick}
+    >
+      <p className="flex justify-center text-[28px] text-center text-black/[0.66]">
+        {text}
+      </p>
+    </button>
+  );
 }
 
 export default GamePlayBtn;
