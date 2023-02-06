8931179fd376777d7a9e3701aade39819249f94d
gofiction95 <gofiction95@gmail.com>
2023-02-07 08:59:08 +0900

    [UPDATE] accessToken 재할당코드작성, facePlay, RiceEat에 logout함수 추가, redux state에 expireTime 추가

--- frontend/src/components/organism/FacePlay/index..jsx
+++ frontend/src/components/organism/FacePlay/index..jsx
@@ -23,7 +23,16 @@ function FacePlay({ children }) {
     console.log("user정보", user);
   };
   const user = useSelector((state) => state.user);
+  const onLogout = async () => {
+    const response = await logoutUser();
 
+    if (parseInt(Number(response.status) / 100) === 2) {
+      return navigate("/");
+    } else {
+      console.log(response);
+    }
+    // input 태그 값 비워주는 코드
+  };
   return (
     <div className="absolute left-48 w-[1076px] h-[100%]">
       <div className="flex justify-end">
@@ -39,7 +48,7 @@ function FacePlay({ children }) {
         ) : (
           <>
             <CommonBtn
-              onClick={logoutUser}
+              onClick={onLogout}
               text={"로그아웃"}
               color="bg-emerald-300"
             />
