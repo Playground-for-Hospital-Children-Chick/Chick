a237f50a7636a90eb394a65e96fdb65addbac872
엄희원 <eom9300@hanmail.net>
2023-02-06 12:59:08 +0900

    [UPDATE] AR BUG test

--- frontend/src/webRTC/Video.jsx
+++ frontend/src/webRTC/Video.jsx
@@ -98,10 +98,11 @@ class Video extends Component {
       !this.state.publisher.stream.videoActive;
     // this.state.publisher.properties.publishVideo =
     //   !this.state.publisher.properties.publishVideo;
-    this.state.session.unpublish(this.state.publisher);
+    this.state.session.unpublish(this.state.publisher).then(() => {
+      this.state.session.publish(this.state.publisher);
+    });
     // newPublisher = OV.initPublisher(this.state.publisher.properties);
     // this.state.session.publish(newPublisher);
-    this.state.session.publish(this.state.publisher);
     // this.newPublish;
   }
 
