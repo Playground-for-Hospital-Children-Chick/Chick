646ec36e33a20d2e33d29aa8b9a287051ef057a4
최정온 <chjo0330@gmail.com>
2023-02-06 23:35:56 +0900

    [UPDATE] 얼굴놀이, 밥 먹기 수정

--- frontend/src/components/molecules/FacePlayHomeBox/index.jsx
+++ frontend/src/components/molecules/FacePlayHomeBox/index.jsx
@@ -12,23 +12,35 @@
       */
 }
 
-import HomeBox from '../../atoms/HomeBox';
-import ChildOne from '../../../assets/images/faceplay/child_one.svg';
-import ChildTwo from '../../../assets/images/faceplay/child_two.svg';
-import ChildThree from '../../../assets/images/faceplay/child_three.svg';
-import ChildFour from '../../../assets/images/faceplay/child_four.svg';
-import LinearProgress from '@mui/material/LinearProgress';
-import GamePlayBtn from './../../atoms/GamePlayBtn/index';
-import Box from '@mui/material/Box';
+import HomeBox from "../../atoms/HomeBox";
+import ChildOne from "../../../assets/images/faceplay/child_one.svg";
+import ChildTwo from "../../../assets/images/faceplay/child_two.svg";
+import ChildThree from "../../../assets/images/faceplay/child_three.svg";
+import ChildFour from "../../../assets/images/faceplay/child_four.svg";
+import LinearProgress from "@mui/material/LinearProgress";
+import GamePlayBtn from "./../../atoms/GamePlayBtn/index";
+import Box from "@mui/material/Box";
 
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function FacePlayHomeBox() {
   let [gameStart, setGameStart] = React.useState(false);
+  const navigate = useNavigate();
+
+  function playTheFaceGame() {
+    setGameStart(true);
+
+    setTimeout(() => {
+      return navigate("/facepage");
+    }, 1500);
+  }
 
   return (
     <HomeBox>
-      <div className="font-chick text-4xl text-center text-black/[0.66]">친구들과 영상 놀이터</div>
+      <div className="font-chick text-4xl text-center text-black/[0.66]">
+        친구들과 영상 놀이터
+      </div>
       <div className="inline-flex justify-center w-[100%]">
         <img src={ChildOne} style={{ height: 87, width: 154 }} />
         <img src={ChildTwo} style={{ height: 87, width: 154 }} />
@@ -41,14 +53,18 @@ function FacePlayHomeBox() {
             친구를 기다리는 중이에요..
           </div>
           <div className="inline-flex justify-center">
-            <Box sx={{ width: '50%' }}>
+            <Box sx={{ width: "50%" }}>
               <LinearProgress />
             </Box>
           </div>
         </>
       ) : (
         <div className="inline-flex justify-center w-[100%]">
-          <GamePlayBtn text={'친구들 만나러 가기'} color="emerald" setGameStart={setGameStart} />
+          <GamePlayBtn
+            text={"친구들 만나러 가기"}
+            color="bg-emerald-300"
+            onClick={playTheFaceGame}
+          />
         </div>
       )}
     </HomeBox>
--- frontend/src/components/molecules/RiceEatHomeBox/index.jsx
+++ frontend/src/components/molecules/RiceEatHomeBox/index.jsx
@@ -12,19 +12,21 @@
       */
 }
 
-import Rice from '../../../assets/images/sidebar/rice.svg';
-import HomeBox from '../../atoms/HomeBox';
-import GamePlayBtn from './../../atoms/GamePlayBtn/index';
+import Rice from "../../../assets/images/sidebar/rice.svg";
+import HomeBox from "../../atoms/HomeBox";
+import GamePlayBtn from "./../../atoms/GamePlayBtn/index";
 
 function RiceEatHomeBox(params) {
   return (
     <HomeBox>
-      <div className="font-chick text-4xl text-center text-black/[0.66]">친구들과 밥 먹기!</div>
+      <div className="font-chick text-4xl text-center text-black/[0.66]">
+        친구들과 밥 먹기!
+      </div>
       <div className="inline-flex justify-center w-[100%]">
         <img src={Rice} style={{ height: 150, width: 150 }} />
       </div>
       <div className="inline-flex justify-center w-[100%]">
-        <GamePlayBtn text={'밥 같이 먹으러 가기'} color="blue" />
+        <GamePlayBtn text={"밥 같이 먹으러 가기"} color="bg-pink-200" />
       </div>
     </HomeBox>
   );
