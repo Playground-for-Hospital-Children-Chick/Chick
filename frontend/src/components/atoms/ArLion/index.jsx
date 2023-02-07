92e492ab05e2a7b69d26ed9581218752269ef86c
gofiction95 <gofiction95@gmail.com>
2023-02-07 20:57:57 +0900

    [UPDATE] 얼굴방 bottom bar우측 사이드바로 조정, 카메라 창 크기 조절

--- frontend/src/components/atoms/ArLion/index.jsx
+++ frontend/src/components/atoms/ArLion/index.jsx
@@ -3,7 +3,7 @@ import Lion from "./../../../assets/images/ar/lion.svg";
 function ArLion() {
   return (
     <>
-      <div className="rounded-full w-[60px] h-[60px] flex items-center justify-center">
+      <div className="rounded-full w-[50px] h-[50px] flex items-center justify-center">
         <img src={Lion} />
       </div>
     </>
