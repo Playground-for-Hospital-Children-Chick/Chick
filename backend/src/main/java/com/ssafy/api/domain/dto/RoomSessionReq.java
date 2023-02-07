d080a5b928dff7260f61f0a36ab2be22605aeccd
juseong <kjschocolate@gmail.com>
2023-02-07 09:56:59 +0900

    [ADD] 자동매칭 RequestDto 추가

---
+++ backend/src/main/java/com/ssafy/api/domain/dto/RoomSessionReq.java
@@ -0,0 +1,16 @@
+package com.ssafy.api.domain.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ApiModel("RoomSessionReq")
+@ToString
+public class RoomSessionReq {
+    @ApiModelProperty(name="유저 email", example = "ssafy@ssafy.com")
+    String email;
+    @ApiModelProperty(name="게임종류", example = "face")
+    String gameType;
+}
