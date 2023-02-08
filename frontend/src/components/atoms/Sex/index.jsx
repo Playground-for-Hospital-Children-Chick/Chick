1ddd381c01fb3aabf01f3c4ba1ee6f6f5db10518
gofiction95 <gofiction95@gmail.com>
2023-02-09 00:39:30 +0900

    [UPDATE] 회원가입 폼 완료, 성별 체크 셀렉트박스 생성

---
+++ frontend/src/components/atoms/Sex/index.jsx
@@ -0,0 +1,18 @@
+function Sex() {
+  return (
+    <div
+      className="text-center font-chick w-[5em]  relative overflow-hidden px-5 py-5 rounded-[30px] bg-yellow-200"
+      style={{ boxShadow: "0px 1px 3px 0 rgba(0,0,0,0.45)" }}
+    >
+      <select className="text-xl bg-yellow-200">
+        {["남", "여"].map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Sex;
