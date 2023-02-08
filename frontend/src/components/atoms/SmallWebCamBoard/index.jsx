67faf72f28a5a8fb1ed23bf1390a8d5c39920cd1
엄희원 <eom9300@hanmail.net>
2023-02-09 01:12:15 +0900

    [UPDATE] Canvas 작업 중

---
+++ frontend/src/components/atoms/SmallWebCamBoard/index.jsx
@@ -0,0 +1,9 @@
+function SmallWebCamBoard({ children }) {
+  return (
+    <div className="grid grid-rows-4 gap-0 w-[400px] h-[700px] overflow-hidden bg-[#133b60] rounded-[30px] place-items-center">
+      {children}
+    </div>
+  );
+}
+
+export default SmallWebCamBoard;
---
+++ frontend/src/components/atoms/TestBox/index.jsx
@@ -0,0 +1,5 @@
+function TestBox({ children }) {
+  return <div className="gird grid-cols-2">{children}</div>;
+}
+
+export default TestBox;
