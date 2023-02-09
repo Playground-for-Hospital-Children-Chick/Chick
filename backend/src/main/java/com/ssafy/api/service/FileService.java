ab98ba9d3f9cf8735c663543e21fdd7cd50d0378
jaeuk <hju9707@naver.com>
2023-02-09 14:11:18 +0900

    [ADD] AWS S3 이미지 업로드 Controller & Service & Model

---
+++ backend/src/main/java/com/ssafy/api/service/FileService.java
@@ -0,0 +1,25 @@
+package com.ssafy.api.service;
+
+import com.ssafy.api.domain.dto.FileDto;
+import com.ssafy.api.domain.entity.FileEntity;
+import com.ssafy.api.domain.repository.FileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+    private final FileRepository fileRepository;
+
+    public void save(FileDto fileDto) {
+        FileEntity fileEntity = new FileEntity(fileDto.getTitle(), fileDto.getUrl());
+        fileRepository.save(fileEntity);
+    }
+
+    public List<FileEntity> getFiles() {
+        List<FileEntity> all = fileRepository.findAll();
+        return all;
+    }
+}
