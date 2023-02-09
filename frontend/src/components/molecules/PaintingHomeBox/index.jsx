4b1a5752f945e3c70b1f2e0109c497541be9df15
hju9707 <hju9707@naver.com>
2023-02-09 22:31:30 +0900

    [ADD] 그림그리기 Room Session 나눔

--- frontend/src/components/molecules/PaintingHomeBox/index.jsx
+++ frontend/src/components/molecules/PaintingHomeBox/index.jsx
@@ -9,6 +9,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Swal from "sweetalert2";
+import io from "socket.io-client";
+const socketRef = useRef();
 function PaintingHomeBox() {
   let [gameStart, setGameStart] = React.useState(false);
   const user = useSelector((state) => state.user);
@@ -24,6 +26,8 @@ function PaintingHomeBox() {
       }).then(() => navigate("/login"));
       return;
     }
+    socketRef = io.connect("ws://i8b207.p.ssafy.io:8001");
+    socketRef.current.emit("join_room", user["userEmail"]);
 
     setGameStart(true);
 
@@ -33,17 +37,13 @@ function PaintingHomeBox() {
   }
   return (
     <HomeBox>
-      <div className="font-chick text-4xl text-center text-black/[0.66]">
-        친구들과 그림 그리기
-      </div>
+      <div className="font-chick text-4xl text-center text-black/[0.66]">친구들과 그림 그리기</div>
       <div className="inline-flex justify-center w-[100%]">
         <img src={Painting} style={{ height: 150, width: 150 }} />
       </div>
       {gameStart ? (
         <>
-          <div className="font-chick text-xl text-center text-black/[0.66]">
-            친구를 기다리는 중이에요..
-          </div>
+          <div className="font-chick text-xl text-center text-black/[0.66]">친구를 기다리는 중이에요..</div>
           <div className="inline-flex justify-center">
             <Box sx={{ width: "50%" }}>
               <LinearProgress />
@@ -52,11 +52,7 @@ function PaintingHomeBox() {
         </>
       ) : (
         <div className="inline-flex justify-center w-[100%]">
-          <GamePlayBtn
-            text={"그림 그리러 가기"}
-            color="bg-yellow-200"
-            onClick={playThePaintingGame}
-          />
+          <GamePlayBtn text={"그림 그리러 가기"} color="bg-yellow-200" onClick={playThePaintingGame} />
         </div>
       )}
     </HomeBox>
