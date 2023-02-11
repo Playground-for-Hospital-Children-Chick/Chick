d60c13ad905bde17b01911b127b208b803260c95
엄희원 <eom9300@hanmail.net>
2023-02-11 20:58:44 +0900

    [UPDATE] 율동 사이드바 크기 조정

--- frontend/src/components/atoms/SmallWebCamBoard/index.jsx
+++ frontend/src/components/atoms/SmallWebCamBoard/index.jsx
@@ -1,6 +1,6 @@
 function SmallWebCamBoard({ children }) {
   return (
-    <div className="grid grid-rows-4 gap-0 w-[400px] h-full overflow-hidden bg-[#133b60] rounded-[30px] place-items-center">
+    <div className="grid grid-rows-4 gap-0 w-[400px] h-[800px] overflow-hidden bg-[#133b60] rounded-[30px] place-items-center mt-3">
       {children}
     </div>
   );
