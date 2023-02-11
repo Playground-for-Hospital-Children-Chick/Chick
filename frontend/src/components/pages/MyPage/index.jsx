3f3f2a4f9f1dd9925e86a862bbf38b173633f573
최정온 <chjo0330@gmail.com>
2023-02-11 16:53:18 +0900

    [UPDATE] 마이페이지 업데이트

--- frontend/src/components/pages/MyPage/index.jsx
+++ frontend/src/components/pages/MyPage/index.jsx
@@ -3,63 +3,47 @@ import { AiOutlineSetting } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import Swal from "sweetalert2";
-import { loginGuest } from "../../../api/UsersApi";
-import { SET_USER, SET_TOKEN } from "../../../store/reducers/UserReducer";
 import { useEffect } from "react";
 
 function MyPage() {
   const user = useSelector((state) => state.user);
 
-  const dispatch = useDispatch();
-
   const navigate = useNavigate();
 
   useEffect(checkLogin, []);
-
-  const onLogin = async () => {
-    const response = await loginGuest();
-
-    if (parseInt(Number(response.status) / 100) === 2) {
-      console.log(response.data.accessToken);
-      dispatch(SET_TOKEN({ accessToken: response.data.accessToken }));
-      dispatch(
-        SET_USER({
-          userEmail: response.data.userLoginInfo.userEmail,
-          userChName: response.data.userLoginInfo.userChName,
-          userAge: response.data.userLoginInfo.userAge,
-          userBirth: response.data.userLoginInfo.userBirth,
-          userSex: response.data.userLoginInfo.userSex,
-          userType: "guest",
-        })
-      );
-    } else {
-      console.log(response);
-    }
-  };
 
   function checkLogin() {
     if (!user["login"]) {
       Swal.fire({
         icon: "info",
         title: "로그인이 필요한 서비스입니다.",
-        text: "내정보를 확인하려면 로그인이 필요합니다.",
-        showDenyButton: true,
-        confirmButtonText: "게스트로 로그인",
-        denyButtonText: `로그인하러가기`,
+        text: "내 정보를 확인하려면 로그인이 필요합니다.",
+        confirmButtonText: "로그인하러가기",
         confirmButtonColor: "#8cc8ff",
-        denyButtonColor: "#ff82b3",
       }).then((result) => {
         if (result.isConfirmed) {
-          console.log("게스트로 로그인");
-          onLogin();
-          return;
-        } else if (result.isDenied) {
           navigate("/login");
           return;
         }
       });
 
-      navigate("/");
+      return;
+    }
+
+    if (user["userType"] === "guest") {
+      Swal.fire({
+        icon: "info",
+        title: "게스트 로그인",
+        text: "내 정보를 확인하려면 사용자 로그인이 필요합니다.",
+        confirmButtonText: "로그인하러가기",
+        confirmButtonColor: "#8cc8ff",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+          return;
+        }
+      });
+
       return;
     }
   }
