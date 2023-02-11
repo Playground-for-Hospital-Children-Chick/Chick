7fb3679e402269068d06324775c8b25e7b670298
최정온 <chjo0330@gmail.com>
2023-02-11 16:23:47 +0900

    [UPDATE] 로그인 페이지/ 로고 추가

--- frontend/src/components/pages/LoginPage/index.jsx
+++ frontend/src/components/pages/LoginPage/index.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { loginUser } from "./../../../api/UsersApi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Logo from "../../atoms/Logo/index";
 
 import { ErrorMessage } from "@hookform/error-message";
 
@@ -64,78 +65,84 @@ function Login() {
     setValue("password", "");
   };
   return (
-    <div className="flex	 justify-center h-screen items-center ">
-      <AlertBox>
-        <div className="top w-21 flex justify-center">
-          <img
-            className="inline after:mr-5 w-1/6"
-            src={chick_02}
-            alt="병아리캐릭터"
-          />
-          <span className="mr-28 text-center inline-block my-10 font-chick text-3xl">
-            로그인
-          </span>
-        </div>
-        <div className="form ">
-          {/* <form className="mt-8 space-y-6" onSubmit={handleSubmit(onLogin)}> */}
-          <form className="mt-8 space-y-6" onSubmit={handleSubmit(onLogin)}>
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div>
-              <div className="mb-10 flex justify-center items-center mr-[5em]">
-                <label className="mr-5 font-chick text-lg" htmlFor="email">
-                  이메일
-                </label>
-                <div className="relative">
-                  <InputBox
-                    register={register("email", {
-                      required: "이메일을 입력하지 않았습니다.",
-                      pattern: {
-                        message: "이메일형식이 잘못되었습니다.",
-                        value:
-                          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
-                      },
-                    })}
-                    // onChange={onEmailChange}
-                    type="text"
-                    placeholder={"이메일을 입력해주세요".toString()}
-                  />
-                  <div className="relatvie w-full">
-                    <ErrorMessage
-                      name="email"
-                      errors={errors}
-                      render={({ message }) =>
-                        message == "이메일을 입력하지 않았습니다." ? (
-                          <div className="absolute top-16 text-md font-chick right-[28%]  text-center text-pink-600">
-                            {message}
-                          </div>
-                        ) : (
-                          <div className="absolute top-16 text-md font-chick left-[30%]  text-center text-pink-600">
-                            {message}
-                          </div>
-                        )
-                      }
+    <>
+      <div className="absolute top-3 left-3 ml-3">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <div className="flex justify-center items-center mt-12 ">
+        <AlertBox>
+          <div className="top w-21 flex justify-center">
+            <img
+              className="inline after:mr-5 w-1/6"
+              src={chick_02}
+              alt="병아리캐릭터"
+            />
+            <span className="mr-28 text-center inline-block my-10 font-chick text-3xl">
+              로그인
+            </span>
+          </div>
+          <div className="form ">
+            {/* <form className="mt-8 space-y-6" onSubmit={handleSubmit(onLogin)}> */}
+            <form className="mt-8 space-y-6" onSubmit={handleSubmit(onLogin)}>
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div>
+                <div className="mb-10 flex justify-center items-center mr-[5em]">
+                  <label className="mr-5 font-chick text-lg" htmlFor="email">
+                    이메일
+                  </label>
+                  <div className="relative">
+                    <InputBox
+                      register={register("email", {
+                        required: "이메일을 입력하지 않았습니다.",
+                        pattern: {
+                          message: "이메일형식이 잘못되었습니다.",
+                          value:
+                            /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
+                        },
+                      })}
+                      // onChange={onEmailChange}
+                      type="text"
+                      placeholder={"이메일을 입력해주세요".toString()}
                     />
+                    <div className="relatvie w-full">
+                      <ErrorMessage
+                        name="email"
+                        errors={errors}
+                        render={({ message }) =>
+                          message == "이메일을 입력하지 않았습니다." ? (
+                            <div className="absolute top-16 text-md font-chick right-[28%]  text-center text-pink-600">
+                              {message}
+                            </div>
+                          ) : (
+                            <div className="absolute top-16 text-md font-chick left-[30%]  text-center text-pink-600">
+                              {message}
+                            </div>
+                          )
+                        }
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="mb-10 flex justify-center items-center mr-[5.5em]">
-                <label className="mr-5 font-chick text-lg" htmlFor="password">
-                  비밀번호
-                </label>
-                <div className="relative">
-                  <InputBox
-                    register={register("password", {
-                      required: "비밀번호를 입력하지 않았습니다.",
-                      // pattern: {
-                      //   message: "비밀번호를 확인해주세요.",
-                      //   value: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/,
-                      // },
-                    })}
-                    // onChange={onEmailChange}
-                    type="password"
-                    placeholder={"암호를 입력해주세요".toString()}
-                  />
-                  {/* <ErrorMessage
+                <div className="mb-10 flex justify-center items-center mr-[5.5em]">
+                  <label className="mr-5 font-chick text-lg" htmlFor="password">
+                    비밀번호
+                  </label>
+                  <div className="relative">
+                    <InputBox
+                      register={register("password", {
+                        required: "비밀번호를 입력하지 않았습니다.",
+                        // pattern: {
+                        //   message: "비밀번호를 확인해주세요.",
+                        //   value: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/,
+                        // },
+                      })}
+                      // onChange={onEmailChange}
+                      type="password"
+                      placeholder={"암호를 입력해주세요".toString()}
+                    />
+                    {/* <ErrorMessage
                     name="password"
                     errors={errors}
                     render={({ message }) =>
@@ -150,25 +157,30 @@ function Login() {
                       )
                     }
                   /> */}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <GamePlayBtn text="로그인" color="bg-emerald-300" type="submit" />
-            </div>
-          </form>
+              <div>
+                <GamePlayBtn
+                  text="로그인"
+                  color="bg-emerald-300"
+                  type="submit"
+                />
+              </div>
+            </form>
 
-          <div className="ml-[2.8em] mt-5 flex justify-center mt-[2.5em]">
-            <span className="mr-10 text-xl font-chick">이메일 찾기</span>
+            <div className="ml-[2.8em] mt-5 flex justify-center mt-[2.5em]">
+              <span className="mr-10 text-xl font-chick">이메일 찾기</span>
 
-            <span className="mr-10 text-xl font-chick">비밀번호 찾기</span>
-            <Link to="/signup">
-              <span className="mr-10 text-xl font-chick">회원가입</span>
-            </Link>
+              <span className="mr-10 text-xl font-chick">비밀번호 찾기</span>
+              <Link to="/signup">
+                <span className="mr-10 text-xl font-chick">회원가입</span>
+              </Link>
+            </div>
           </div>
-        </div>
-      </AlertBox>
-    </div>
+        </AlertBox>
+      </div>
+    </>
   );
 }
 
