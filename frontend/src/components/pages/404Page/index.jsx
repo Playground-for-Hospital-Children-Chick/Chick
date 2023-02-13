44408698353227ff76b4e0c93711beca56e9e690
gofiction95 <gofiction95@gmail.com>
2023-02-13 16:55:47 +0900

    [ADD] 회원가입 css 수정, 404page 생성

---
+++ frontend/src/components/pages/404Page/index.jsx
@@ -0,0 +1,15 @@
+import AlertBox from "./../../atoms/AlertBox/index";
+function NotFound() {
+  return (
+    <>
+      <div>
+        <AlertBox>
+          <div></div>
+          <img src="" alt="" />
+        </AlertBox>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
