c19f243a51ee45de694b5e5a9b0adcf2df797616
최정온 <chjo0330@gmail.com>
2023-02-13 13:53:46 +0900

    [UPDATE] 게임 화면 버튼 애니메이션 추가

--- frontend/src/components/pages/CartoonPage/index.jsx
+++ frontend/src/components/pages/CartoonPage/index.jsx
@@ -23,14 +23,20 @@ function CartoonPage() {
   return (
     <>
       <div className="flex align-middle justify-center w-[100%] h-[100%] mt-7 mb-4">
-        <button onClick={goBeforePage} className="mr-12">
+        <button
+          onClick={goBeforePage}
+          className="mr-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150"
+        >
           <span className="mr-8">
             <div className="font-chick text-xl">이전화 보기</div>
             <AiOutlineArrowLeft size={110} />
           </span>
         </button>
         <Cartoon index={index} />
-        <button onClick={goNextPage} className="ml-12">
+        <button
+          onClick={goNextPage}
+          className="ml-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150"
+        >
           <span className="ml-8">
             <div className="font-chick text-xl">다음화 보기</div>
             <AiOutlineArrowRight size={110} />
