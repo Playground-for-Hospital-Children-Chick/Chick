cec4966239a5623067cea79ff6283c74be75f09a
jaeuk <hju9707@naver.com>
2023-02-13 13:17:47 +0900

    [UPDATE] 출석일 수 distinct 정상 출력

--- backend/src/main/java/com/ssafy/api/domain/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/api/domain/repository/UserRepository.java
@@ -23,7 +23,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value="UPDATE User u SET u.userState=1 where u.userEmail=?1")
     void deleteByEmail(String userEmail);
 
-    @Query(nativeQuery = true, value = "SELECT DISTINCT count(date_format(mat_create_date,  '%y-%m-%d'))as attendance_day from matching where mat_create_by= :email")
+    @Query(nativeQuery = true, value = "select count(distinct(date_format(mat_create_date,  '%y-%m-%d')))as attendance_day from matching where mat_create_by= :email")
+
     int getAttendanceDay(String email);
 
 }
