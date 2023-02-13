86abdf014d1c516ea246161c591b2fe986899b2b
최정온 <chjo0330@gmail.com>
2023-02-14 01:48:26 +0900

    [UPDATE] 반응형 UI 테스트

--- frontend/src/components/atoms/WebCamBoard/index.jsx
+++ frontend/src/components/atoms/WebCamBoard/index.jsx
@@ -22,7 +22,8 @@ default="blue"
 
 function WebCamBoard({ children }) {
   return (
-    <div className="grid grid-cols-2 mt-3 mb-2 gap-0 w-[1050px] h-[699px] overflow-hidden bg-[#133b60] rounded-[30px]">
+    // <div className="grid grid-cols-2 mt-3 mb-2 gap-0 w-[1050px] h-[699px] overflow-hidden bg-[#133b60] rounded-[30px]">
+    <div className="grid grid-cols-2 mt-3 mb-2 gap-0 w-[500px] h-[700px] md:w-[620px] lg:w-[880px] xl:w-[1140px] 2xl:w-[1400px] overflow-hidden bg-[#133b60] rounded-[30px]">
       {children}
     </div>
   );
