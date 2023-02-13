c862878d3b425560b8428dc1a79f13961d004f5c
최정온 <chjo0330@gmail.com>
2023-02-13 09:28:35 +0900

    [UPDATE] 만화 추가

--- frontend/src/components/pages/CartoonPage/index.jsx
+++ frontend/src/components/pages/CartoonPage/index.jsx
@@ -1,7 +1,25 @@
+import CartoonOne from "../../../assets/cartoon/cartoon_01.png";
+import CartoonTwo from "../../../assets/cartoon/cartoon_02.png";
+import CartoonThree from "../../../assets/cartoon/cartoon_03.png";
+import CartoonFour from "../../../assets/cartoon/cartoon_04.png";
+import CartoonFive from "../../../assets/cartoon/cartoon_05.png";
+import CartoonSix from "../../../assets/cartoon/cartoon_06.png";
+import CartoonSeven from "../../../assets/cartoon/cartoon_07.png";
+import CartoonEight from "../../../assets/cartoon/cartoon_08.png";
+
 function CartoonPage() {
   return (
     <>
-      <div>카툰 페이지</div>
+      <div className="flex h-64 justify-center align-center mt-6">
+        <div className="text-center">
+          <div className="inline-flex justify-center w-[100%] mt-7 mb-4">
+            <img
+              className="w-4/12 border-2 border-black border-opacity-75"
+              src={CartoonOne}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
