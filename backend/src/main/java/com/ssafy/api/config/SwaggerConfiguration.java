247b84559f9737d5ac2123326e5a6209cc243ba8
hju9707 <hju9707@naver.com>
2023-02-15 07:39:38 +0900

    [ADD] swgger springsecurity auth 추가

--- backend/src/main/java/com/ssafy/api/config/SwaggerConfiguration.java
+++ backend/src/main/java/com/ssafy/api/config/SwaggerConfiguration.java
@@ -9,13 +9,17 @@ import com.google.common.base.Predicates;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration // 스프링 실행시 설정파일
 @EnableSwagger2 // Swagger2를 사용
@@ -46,11 +50,34 @@ public class SwaggerConfiguration {
 //		responseMessages.add(new ResponseMessageBuilder().code(404).message("페이지를 찾을 수 없습니다 !!!").build());
         return new Docket(DocumentationType.SWAGGER_2).groupName(groupName).apiInfo(apiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy.api.controller")).paths(predicate)
-                .apis(RequestHandlerSelectors.any()).build();
+                .apis(RequestHandlerSelectors.any()).build()
+                .securityContexts(newArrayList(securityContext()))
+                .securitySchemes(newArrayList(apiKey()));
 //				.useDefaultResponseMessages(false)
 //				.globalResponseMessage(RequestMethod.GET,responseMessages);
     }
+    private ApiKey apiKey() {
+        return new ApiKey(SECURITY_SCHEMA_NAME, "Authorization", "header");
+    }
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+    public static final String SECURITY_SCHEMA_NAME = "JWT";
+    public static final String AUTHORIZATION_SCOPE_GLOBAL = "global";
+    public static final String AUTHORIZATION_SCOPE_GLOBAL_DESC = "accessEverything";
 
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(AUTHORIZATION_SCOPE_GLOBAL, AUTHORIZATION_SCOPE_GLOBAL_DESC);
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(new SecurityReference(SECURITY_SCHEMA_NAME, authorizationScopes));
+    }
     // swagger ui 설정.
     @Bean
     public Docket allApi() {
