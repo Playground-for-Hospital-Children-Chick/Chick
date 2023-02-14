ad40863a7a96880c6a530ae4bcf4b5eccfba1928
juseong <kjschocolate@gmail.com>
2023-02-14 16:40:33 +0900

    [ADD] 만화보기 첫페이지 이전화 보기 안보이기, 마지막페이지 다음화 보기 안보이기 추가

--- frontend/src/components/pages/CartoonPage/index.jsx
+++ frontend/src/components/pages/CartoonPage/index.jsx
@@ -23,23 +23,17 @@ function CartoonPage() {
   return (
     <>
       <div className="flex align-middle justify-center w-[100%] h-[100%] mt-7 mb-4">
-        <button
-          onClick={goBeforePage}
-          className="mr-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150"
-        >
+        <button onClick={goBeforePage} className="mr-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150">
           <span className="mr-8">
-            <div className="font-chick text-xl">이전화 보기</div>
-            <AiOutlineArrowLeft size={110} />
+            {index > 0 ? <div className="font-chick text-xl">이전화 보기</div> : <div className="font-chick text-xl invisible">이전화 보기</div>}
+            {index > 0 ? <AiOutlineArrowLeft size={110} /> : <AiOutlineArrowLeft size={110} className="invisible" />}
           </span>
         </button>
         <Cartoon index={index} />
-        <button
-          onClick={goNextPage}
-          className="ml-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150"
-        >
+        <button onClick={goNextPage} className="ml-12 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150">
           <span className="ml-8">
-            <div className="font-chick text-xl">다음화 보기</div>
-            <AiOutlineArrowRight size={110} />
+            {index < 10 ? <div className="font-chick text-xl"> 다음화 보기</div> : <div className="font-chick text-xl invisible">다음화 보기</div>}
+            {index < 10 ? <AiOutlineArrowRight size={110} /> : <AiOutlineArrowRight size={110} className="invisible" />}
           </span>
         </button>
       </div>
